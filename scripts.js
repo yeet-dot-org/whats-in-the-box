71fbbaad299f756d124c783677ee64491d1e0c66
the fitnessgram pacer test is a multistage aerobic capacity test <71843354+yeet-dot-org@users.noreply.github.com>
2021-11-23 14:46:38 -0600

    Nov.23

---
+++ scripts.js
@@ -0,0 +1,9 @@
+function findOut(){
+    document.getElementById('image_1').style.display = 'none';
+    document.getElementById('image_2').style.display = 'block';
+}
+
+function playAgain(){
+    document.getElementById('image_1').style.display = 'block';
+    document.getElementById('image_2').style.display = 'none';
+}
